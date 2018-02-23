a04f7bc4108458a1da12b675cd5b307b24a27f4e
John Kennedy (codejockie) <devjckennedy@gmail.com>
2018-02-23 17:17:58 +0100

    (chore): add extract text plugin for css change build directory to dist and delete src directory

--- __tests__/server/controllers/search.spec.js
+++ __tests__/server/controllers/search.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import supertest from 'supertest';
-import app from '../../../src/server';
+import app from '../../../dist/server';
 import models from '../../../server/models';
 
 const { Document, Role, User } = models;
