cdac5bfeb7219a181ca58c95a41301602aa7a4cd
John C. Kennedy <devjckennedy@gmail.com>
2018-10-14 14:15:26 +0900

    remove username check in findByToken method

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -46,8 +46,6 @@ export function findByToken(token) {
           id: decoded.id
         }, {
           email: decoded.email
-        }, {
-          username: decoded.username
         }]
       }
     });
