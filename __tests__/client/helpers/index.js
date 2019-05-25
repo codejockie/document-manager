c232420e272ba8f65dc717bcd40bcc4d53b9dec7
John C. Kennedy <devjckennedy@gmail.com>
2019-05-25 10:33:06 +0900

    update header component test to 100% coverage

--- __tests__/client/helpers/index.js
+++ __tests__/client/helpers/index.js
@@ -1,27 +1,37 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
 import Adapter from 'enzyme-adapter-react-16';
+import { withStyles } from '@material-ui/core/styles';
+
+import styles from './styles';
 
 configure({ adapter: new Adapter() });
 
-const middleware = [];
+const wrapRouter = (Component, initialState, props) => {
+  const middleware = [];
+  const mockStore = configureStore(middleware);
+  const store = mockStore(initialState);
+  // Wrap component with Material UI styling
+  const Composition = withStyles(styles)(Component);
+  return (
+    <BrowserRouter>
+      <Composition store={store} {...props} />
+    </BrowserRouter>
+  );
+};
 
 /**
  * @param { object } Component React component
- * @param { object } initialState Component store initial state
+ * @param { object } options Options to configure component rendering
  * @returns { object } props and component
  */
-function setup(Component, initialState = {}) {
-  const mockStore = configureStore(middleware);
-  const store = mockStore(initialState);
-
-  const component = mount((
-    <BrowserRouter>
-      <Component store={store} />
-    </BrowserRouter>
-  ));
+function setup(Component, options) {
+  const { initialState = {}, props = {}, isShallow = false } = options;
+  const component = isShallow
+    ? shallow(wrapRouter(Component, initialState, props))
+    : mount(wrapRouter(Component, initialState, props));
 
   return {
     component
