559eded30d94fd5f9c82f8494444e5d0709c98ea
John C. Kennedy <devjckennedy@gmail.com>
2020-06-17 15:28:04 +0900

    gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
-import exit from 'gulp-exit';
-import injectModules from 'gulp-inject-modules';
-import istanbul from 'gulp-istanbul';
-import jasmine from 'gulp-jasmine';
 
 const compile = (cb) => {
   gulp.src('server/**/*.js')
@@ -19,23 +15,4 @@ const compileTest = (cb) => {
   cb();
 };
 
-const test = (cb) => {
-  gulp.src(['dist/controllers/*.js', 'dist/helpers/*.js', 'dist/middleware/*.js'])
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', () => {
-      gulp.src('./__tests__/**/*.js')
-        .pipe(babel())
-        .pipe(injectModules())
-        .pipe(jasmine({
-          verbose: true
-        }))
-        .pipe(istanbul.writeReports())
-        .pipe(istanbul.enforceThresholds({ thresholds: { global: 85 } }))
-        .on('end', cb)
-        .pipe(exit());
-    });
-  cb();
-};
-
-exports.build = gulp.series(test, gulp.parallel(compile, compileTest));
+exports.build = gulp.series(gulp.parallel(compile, compileTest));
